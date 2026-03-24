7f9ef1f019ebe648542f9c100a2765395b04d8fc
Dave Paroulek <david.paroulek@camunda.com>
2026-03-24 11:25:11 -0400

    few small additions

--- src/main/java/org/example/camunda/process/solution/worker/MyWorker.java
+++ src/main/java/org/example/camunda/process/solution/worker/MyWorker.java
@@ -1,7 +1,10 @@
 package org.example.camunda.process.solution.worker;
 
+import io.camunda.client.CamundaClient;
 import io.camunda.client.annotation.JobWorker;
 import io.camunda.client.annotation.VariablesAsType;
+import io.camunda.client.api.response.ActivatedJob;
+import io.camunda.client.api.worker.JobClient;
 import org.example.camunda.process.solution.ProcessVariables;
 import org.example.camunda.process.solution.service.MyService;
 import org.slf4j.Logger;
@@ -14,12 +17,14 @@ public class MyWorker {
   private static final Logger LOG = LoggerFactory.getLogger(MyWorker.class);
 
   private final MyService myService;
+  private final CamundaClient camundaClient;
 
-  public MyWorker(MyService myService) {
+  public MyWorker(MyService myService, CamundaClient camundaClient) {
     this.myService = myService;
+    this.camundaClient = camundaClient;
   }
 
-  @JobWorker
+  @JobWorker(type = "myService")
   public ProcessVariables invokeMyService(@VariablesAsType ProcessVariables variables) {
     LOG.info("Invoking myService with variables: " + variables);
 
@@ -27,5 +32,36 @@ public class MyWorker {
 
     return new ProcessVariables()
         .setResult(result); // new object to avoid sending unchanged variables
+  }
+
+  @JobWorker(type = "runMyService", timeout = 1000, maxRetries = 1)
+  public void runMyService(JobClient jobClient, ActivatedJob job) {
+
+    // Tell the engine we need more time
+    camundaClient.newUpdateJobCommand(job)
+        .updateTimeout(5000)
+        .send()
+        .join();
+
+    try {
+
+      // Perform business logic here
+
+      jobClient
+          .newCompleteCommand(job.getKey())
+          .send()
+          .join();
+
+    } catch (Exception e) {
+
+      // Tell the engine that the job failed and we don't want to retry
+
+      jobClient
+          .newFailCommand(job.getKey())
+          .retries(1)
+          .errorMessage(e.getMessage())
+          .send()
+          .join();
+    }
   }
 }
